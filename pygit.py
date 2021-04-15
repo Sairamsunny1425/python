4cb48e0fc15f61150e42d684b1c0324d14415e70
Sairamsunny1425 <chenapathi.sai.16cse@bml.edu.in>
2021-04-15 12:34:01 +0530

    from local machine again

--- pygit.py
+++ pygit.py
@@ -1,9 +1,5 @@
 x=4
 print(x+4)
 print('commit from local machine')
-<<<<<<< HEAD
-print('from git')
-print('second commit from local machine')
-=======
 print('from git-hub')
->>>>>>> 9378e9108e89bf7edf788671a68b9085e2b9120f
+print('second commit from local machine')
